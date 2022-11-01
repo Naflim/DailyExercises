a34c6e7b637a98fb115c8eee7b82c44b6abbd1a7
naflim <naflim@outlook.com>
2022-11-01 18:32:30 +0800

    和至少为 K 的最短子数组

--- ShortestArray4K.cs
+++ ShortestArray4K.cs
@@ -15,42 +15,129 @@ namespace DailyExercises
     {
         public static int ShortestSubarray(int[] nums, int k)
         {
-            var sortDescending = SortDescending(nums);
-            int max = nums.Max();
-            if (max >= k) return 1;
-
-            for (int i = k / max + 1; i <= nums.Length; i++)
+            int n = nums.Length;
+            long[] sums = new long[n + 1];
+            for (int i = 1; i <= n; i++)
             {
-                for(int j = sortDescending.Count - 1; j >= 0; j--)
-                {
-                    var num = sortDescending.ElementAt(j).Key;
-                    if (num * i < k) break;
-                    var indexs = sortDescending.ElementAt(j).Value;
-                    for(int k1 = 0; k1 < indexs.Count; k1++)
-                    {
-                        int head;
-                        if (indexs[k1] >= i-1)
-                        {
-                            head = indexs[k1] - i + 1;
-                            //if (nums.Skip(head).Take(i).Sum() >= k) return i;
-                        }
-                        else head = 0;
-
-                        for(int l = head; l<=indexs[k1]; l++)
-                        {
-                            if (nums.Skip(l).Take(i).Sum() >= k) return i;
-                        }
-
-                        //if (indexs[k1] <= nums.Length - i)
-                        //{
-                        //    //if (nums.Skip(indexs[k1]).Take(i).Sum() >= k) return i;
-                        //}
-                    }
-                }
+                sums[i] = sums[i - 1] + nums[i - 1];
             }
-
-            return -1;
+            LinkedList<int> list = new ();
+            list.AddFirst(0);
+            int ans = -1;
+            for (int i = 1; i <= n; i++)
+            {
+                long pre = sums[i];
+                LinkedListNode<int> node = list.First;
+                while (node != null && pre - sums[node.Value] >= k)
+                {
+                    if (ans == -1) ans = i - node.Value;
+                    else ans = Math.Min(ans, i - node.Value);
+                    list.RemoveFirst();
+                    node = list.First;
+                }
+                node = list.Last;
+                while (node != null && pre <= sums[node.Value])
+                {
+                    list.RemoveLast();
+                    node = list.Last;
+                }
+                list.AddLast(i);
+            }
+            return ans;
         }
+
+        //public static int ShortestSubarray(int[] nums, int k)
+        //{
+        //    if (nums.Max() >= k) return 1;
+        //    int shortCount = int.MaxValue;
+        //    List<int> section = new List<int>();
+        //    List<int> sectionMap = new List<int>();
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        var num = nums[i];
+        //        if (num > 0) 
+        //        {
+        //            section.Add(num);
+        //            sectionMap.Add(1);
+        //        }
+        //        else
+        //        {
+        //            if (section.Sum() + num <= 0)
+        //            {
+        //                section.Clear();
+        //                sectionMap.Clear();
+        //            }
+        //            else Compress(section, sectionMap, num);
+        //        }
+
+        //        int sum = section.Sum();
+        //        if (sum >= k) 
+        //        {
+        //            int shortNum = sectionMap.Sum();
+        //            if (sum > k)
+        //            {
+        //                while (true)
+        //                {
+        //                    if (section.Skip(1).Take(section.Count - 1).Sum() < k) 
+        //                    {
+        //                        section.RemoveAt(0);
+        //                        sectionMap.RemoveAt(0);
+        //                        break;
+        //                    }
+        //                    else
+        //                    {
+        //                        shortNum -= sectionMap[0];
+        //                        section.RemoveAt(0);
+        //                        sectionMap.RemoveAt(0);
+        //                    }
+        //                }
+        //            }
+        //            shortCount = Math.Min(shortCount, shortNum);
+        //        }
+        //    }
+
+        //    if (shortCount != int.MaxValue) return shortCount;
+        //    else return -1;
+        //}
+
+        //public static int ShortestSubarray(int[] nums, int k)
+        //{
+        //    var sortDescending = SortDescending(nums);
+        //    var sortDic = SortDescending(sortDescending);
+
+        //    int len = nums.Length;
+        //    int max = nums.Max();
+
+        //    int shortCount = int.MaxValue;
+        //    if (max >= k) return 1;
+
+        //    while (sortDic.Count > 0)
+        //    {
+        //        var val = sortDic.Last();
+        //        int i,count = 0;
+        //        for (i = val.Key; i < len && count < k; i++)
+        //        {
+        //            count += nums[i];
+        //        }
+
+        //        if(count < k)
+        //        {
+        //            for (i = val.Key; i < len; i++)
+        //            {
+        //                if (sortDic.ContainsKey(i)) sortDic.Remove(i);
+        //                else break;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            shortCount = Math.Min(shortCount, i - val.Key);
+        //            sortDic.Remove(val.Key);
+        //        }
+        //    }
+
+        //    if(shortCount != int.MaxValue)return shortCount;
+        //    else return -1;
+        //}
 
         public static SortedDictionary<int,List<int>> SortDescending(int[] num)
         {
@@ -62,6 +149,38 @@ namespace DailyExercises
             }
 
             return sortedDictionary;
+        }
+
+        public static Dictionary<int, int> SortDescending(SortedDictionary<int, List<int>> sortDec)
+        {
+            Dictionary<int, int> sortedDictionary = new Dictionary<int, int>();
+            foreach(var item in sortDec)
+            {
+                item.Value.ForEach(i => sortedDictionary.Add(i, item.Key));
+            }
+
+            return sortedDictionary;
+        }
+
+        public static void Compress(List<int> section,List<int> map,int data)
+        {
+            int last = section.Count - 1;
+            int compressData = section[last] + data;
+
+            if(compressData > 0)
+            {
+                section[last] = compressData;
+                map[last]++;
+            }
+            else
+            {
+                int newLast = last - 1;
+                section[newLast] +=  section[last];
+                map[newLast] +=  map[last];
+                section.RemoveAt(last);
+                map.RemoveAt(last);
+                Compress(section, map, data);
+            }
         }
     }
 }
