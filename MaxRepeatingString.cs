ab768de0d2bdeec7b984d5d853c3a67771e4167c
naflim <naflim@outlook.com>
2022-11-03 10:24:31 +0800

    1668. 最大重复子字符串

---
+++ MaxRepeatingString.cs
@@ -0,0 +1,32 @@
+﻿namespace DailyExercises
+{
+    /// <summary>
+    /// 1668. 最大重复子字符串
+    /// </summary>
+    public class MaxRepeatingString
+    {
+        public static int MaxRepeating(string sequence, string word)
+        {
+            int k = 0;
+            int wordLen = word.Length;
+            int section = sequence.Length - wordLen + 1;
+            int count = 0;
+            for (int i = 0; i<section; i++)
+            {
+                string sub = new(sequence.Skip(i).Take(wordLen).ToArray());
+                if (sub == word)
+                {
+                    count++;
+                    i += wordLen - 1;
+                }
+                else if (count != 0)
+                {
+                    k = Math.Max(count, k);
+                    count = 0;
+                    i -= wordLen - 1;
+                }
+            }
+            return Math.Max(count, k);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,23 +4,7 @@ class Program
 {
     public static void Main()
     {
-        var demo = BestNetworkSignal.BestCoordinate(new int[][]
-        {
-            new int[]{ 30,34,31 },
-            new int[]{10,44,24},
-            new int[]{14,28,23},
-             new int[]{ 50, 38, 1 },
-             new int[]{ 40,13,6 },
-              new int[]{ 16,27,9 },
-               new int[]{ 2,22,23 },
-                new int[]{ 1,6,41 },
-                 new int[]{ 34,22,40 },
-                  new int[]{40,10,11 },
-        }, 20);
-        for (int i = 0; i <demo.Length; i++)
-        {
-            Console.WriteLine(demo[i]);
-        }
+        Console.WriteLine(MaxRepeatingString.MaxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba", "aaaba"));
         Console.Read();
     }
 }
