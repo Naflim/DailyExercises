1f4865997179ec62b38b34d18990100dd6ed9b48
naflim <naflim@outlook.com>
2022-11-16 12:41:39 +0800

    775. 全局倒置与局部倒置

---
+++ IdealPermutation.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    internal class IdealPermutation
+    {
+        public static bool IsIdealPermutation(int[] nums)
+        {
+            int max = 0;
+            for (int i = 0; i<nums.Length - 1; i++)
+            {
+                if (max>nums[i+1]) return false;
+
+                max = Math.Max(max, nums[i]);
+                //if (nums[i]>max) max = nums[i];
+            }
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,17 +4,7 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(MaximumUnitsNum.MaximumUnits(new int[][]
-        {
-            new int[]{1,3},
-             new int[]{5,5},
-              new int[]{ 2,5 },
-              new int[]{ 4,2 },
-               new int[]{ 4,1 },
-                new int[]{3,1 }, new int[]{ 2,2 },
-                new int[]{ 1,3 },
-                new int[]{ 2,5},new int[]{ 3, 2 }
-        }, 35));
+        Console.WriteLine(IdealPermutation.IsIdealPermutation(new int[] { 1, 0, 2 }));
 
         Console.ReadLine();
     }
