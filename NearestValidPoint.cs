99e0dc76891ebe3101e6dbaf95fe04242349a80f
naflim <naflim@outlook.com>
2022-12-01 09:51:20 +0800

    1779. 找到最近的有相同 X 或 Y 坐标的点

---
+++ NearestValidPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1779. 找到最近的有相同 X 或 Y 坐标的点
+    /// </summary>
+    internal class NearestValidPoint
+    {
+        public static int Run(int x, int y, int[][] points)
+        {
+            int index = -1, distance = int.MaxValue;
+            for (int i = 0; i<points.Length; i++)
+            {
+                var point = points[i];
+                if (point[0] == x)
+                {
+                    var nowDis = Math.Abs(y - point[1]);
+                    if (nowDis < distance)
+                    {
+                        index = i;
+                        distance = nowDis;
+                    }
+
+                }
+                else if (point[1] == y)
+                {
+                    var nowDis = Math.Abs(x - point[0]);
+                    if (nowDis < distance)
+                    {
+                        index = i;
+                        distance = nowDis;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(MinOperations.Run("1111"));
+        Console.WriteLine(NearestValidPoint.Run(3,4,new int[][] 
+        {
+            new int[]{2,3},
+            //new int[]{3,1},
+            //new int[]{2,4},
+            //new int[]{2,3},
+            //new int[]{4,4},
+        }));
         Console.Read();
     }
 }
