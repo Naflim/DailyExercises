b346b9717a07436af1cbc426164c1a5327598615
naflim <naflim@outlook.com>
2022-12-06 10:02:17 +0800

    1805. 字符串中不同整数的数目

---
+++ NumDifferentIntegers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    internal class NumDifferentIntegers
+    {
+        public static int Run(string word)
+        {
+            List<string> nums = new();
+
+            string buffer = string.Empty;
+            foreach (var c in word)
+            {
+                if ('0'<=c&&c<='9')
+                {
+                    buffer += c;
+                }
+                else
+                {
+                    if (buffer != string.Empty)
+                    {
+                        int i;
+                        for (i=0; i<buffer.Length; i++)
+                        {
+                            if (buffer[i] != '0') break;
+                        }
+
+                        nums.Add(new string(buffer.Skip(i).Take(buffer.Length - i).ToArray()));
+                        buffer = string.Empty;
+                    }
+                }
+            }
+
+            if (buffer != string.Empty)
+            {
+                int i;
+                for (i=0; i<buffer.Length; i++)
+                {
+                    if (buffer[i] != '0') break;
+                }
+
+                nums.Add(new string(buffer.Skip(i).Take(buffer.Length - i).ToArray()));
+            }
+
+            return nums.Distinct().Count();
+        }
+    }
+}
