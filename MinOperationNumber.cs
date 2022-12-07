538617cb0859fbb28b33855ded914fb44012c9a1
naflim <naflim@outlook.com>
2022-12-07 11:30:08 +0800

    1775. 通过最少操作次数使数组的和相等

---
+++ MinOperationNumber.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1775. 通过最少操作次数使数组的和相等
+    /// </summary>
+    internal class MinOperationNumber
+    {
+        public static int Run(int[] nums1, int[] nums2)
+        {
+            List<int> big, small;
+            if (nums1.Sum()>nums2.Sum())
+            {
+                big = nums1.ToList();
+                small = nums2.ToList();
+            }
+            else if (nums1.Sum()<nums2.Sum())
+            {
+                big = nums2.ToList();
+                small = nums1.ToList();
+            }
+            else return 0;
+
+            List<int> operations = new List<int>();
+
+            big.ForEach(v=>operations.Add(v - 1));
+            small.ForEach(v=>operations.Add(6 - v));
+
+            operations.Sort();
+            operations.Reverse();
+
+            int disparity = big.Sum() - small.Sum();
+
+            int i = 0,count = 0;
+            for (; i<operations.Count; i++)
+            {
+                count += operations[i];
+                if (count >= disparity) break;
+            }
+
+            if (i == operations.Count) return -1;
+            else return i + 1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(NumDifferentIntegers.Run("a123bc34d8ef34"));
+        Console.WriteLine(MinOperationNumber.Run(new int[] { 2, 3 }, new int[] { 5 }));
     }
 }
