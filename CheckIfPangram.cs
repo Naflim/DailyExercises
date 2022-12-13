81b0e33d16db17627dbf677116eaa830e2394c9d
naflim <naflim@outlook.com>
2022-12-13 09:52:39 +0800

    1832. 判断句子是否为全字母句

---
+++ CheckIfPangram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    internal class CheckIfPangram
+    {
+        public static bool Run(string sentence)
+        {
+            string table = string.Empty;
+
+            foreach(var c in sentence) 
+            {
+                if(!table.Contains(c)) table += c;
+            }
+
+            return table.Length == 26;
+        }
+    }
+}
