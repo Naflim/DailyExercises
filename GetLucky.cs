3ca1caf04031e2c6b82dc69ee2c4444323ff1ec5
naflim <naflim@outlook.com>
2022-12-16 09:29:44 +0800

    1945. 字符串转化后的各位数字之和

---
+++ GetLucky.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1945. 字符串转化后的各位数字之和
+    /// </summary>
+    internal class GetLucky
+    {
+        public static int Run(string s, int k)
+        {
+            string strNum = string.Empty;
+
+            foreach (char c in s)
+            {
+                strNum += c - 96;
+            }
+
+            for(int i = 0; i<k; i++)
+            {
+                strNum = StringNumSum(strNum);
+            }
+
+            return Convert.ToInt32(strNum);
+        }
+
+        public static string StringNumSum(string s)
+        {
+            int count = 0;
+            foreach (char c in s)
+            {
+                count += c - 48;
+            }
+
+            return count.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿using DailyExercises;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using System.Text;
 
 class Program
 {
     public static void Main()
     {
-        Console.WriteLine(CheckIfPangram.Run("thequickbrownfoxjumpsoverthelazydog"));
+        List<int> list = new List<int> { 1,2,3};
+        ObservableCollection<int> ints= new ObservableCollection<int>(list);
+        ints.RemoveAt(0);
+        Console.WriteLine(list.Count);
+        Console.WriteLine(ints.Count);
+        Console.ReadLine();
     }
 }
