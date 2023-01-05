01417209879e8dc3bcf3c154e6dbc5a6a63f4b47
naflim <naflim@outlook.com>
2023-01-05 13:43:51 +0800

    1803. 统计异或值在范围内的数对有多少

---
+++ CountPairs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1803. 统计异或值在范围内的数对有多少
+    /// </summary>
+    internal class CountPairs
+    {
+        public static int Run(int[] nums, int low, int high)
+        {
+            Array.Sort(nums);
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i+1; j<nums.Length; j++) 
+                {
+                    var xor = nums[i]^nums[j];
+                    if (xor >= low && xor <= high) count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(SubscriptMaximum.Run(6, 2, 931384943));
+        Console.WriteLine(CountPairs.Run(new int[] { 9, 8, 4, 2, 1 },5,14));
+        Console.WriteLine(2^7);
         Console.ReadLine();
     }
 }
