16ebf6e72ae41581483357a166d8088430669e83
naflim <naflim@outlook.com>
2023-01-30 10:29:53 +0800

    1669. 合并两个链表

---
+++ MergeInBetween.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1669. 合并两个链表
+    /// </summary>
+    internal class MergeInBetween
+    {
+        public static ListNode Run(ListNode list1, int a, int b, ListNode list2)
+        {
+            ListNode? start;
+
+            if (a < 0) throw new IndexOutOfRangeException("参数a不能小于0");
+            else if (a == 0) start = null;
+            else start = list1;
+
+            for (int i = 1; i < a; i++)
+            {
+                start = start.next;
+            }
+
+            if (b < a) throw new IndexOutOfRangeException("参数b不能小于a");
+
+            ListNode end = start?.next.next??list1.next;
+
+            for(int i = 0;i< b - a; i++)
+            {
+                end = end.next;
+            }
+
+            start.next = list2;
+            GetLastNode(list2).next = end;
+
+            return list1;
+        }
+
+        public static ListNode? GetListNode(IEnumerable<int> nums)
+        {
+            ListNode? head = null;
+            ListNode? node = null;
+
+            foreach (var num in nums)
+            {
+                if (node == null) head = node = new ListNode(num);
+                else
+                {
+                    node.next = new ListNode(num);
+                    node = node.next;
+                }
+            }
+
+            return head;
+        }
+
+        public static ListNode GetLastNode(ListNode node)
+        {
+            while (node.next != null) node = node.next;
+
+            return node;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,20 +8,13 @@ class Program
 {
     public static void Main()
     {
-        MKAverage obj = new MKAverage(3, 1);
-        obj.AddElement(3);        // 当前元素为 [3]
-        obj.AddElement(1);        // 当前元素为 [3,1]
-        Console.WriteLine(obj.CalculateMKAverage()); // 返回 -1 ，因为 m = 3 ，但数据流中只有 2 个元素
-        obj.AddElement(10);       // 当前元素为 [3,1,10]
-        Console.WriteLine(obj.CalculateMKAverage()); // 最后 3 个元素为 [3,1,10]
-                                                     // 删除最小以及最大的 1 个元素后，容器为 [3]
-                                                     // [3] 的平均值等于 3/1 = 3 ，故返回 3
-        obj.AddElement(5);        // 当前元素为 [3,1,10,5]
-        obj.AddElement(5);        // 当前元素为 [3,1,10,5,5]
-        obj.AddElement(5);        // 当前元素为 [3,1,10,5,5,5]
-        Console.WriteLine(obj.CalculateMKAverage()); // 最后 3 个元素为 [5,5,5]
-                                                     // 删除最小以及最大的 1 个元素后，容器为 [5]
-                                                     // [5] 的平均值等于 5/1 = 5 ，故返回 5
 
+        var val = MergeInBetween.Run(MergeInBetween.GetListNode(new int[] { 0, 1, 2, 3, 4, 5, 6 }), 2, 5, MergeInBetween.GetListNode(new int[] { 1000000, 1000001, 1000002, 1000003, 1000004 }));
+
+        while (val != null) 
+        {
+            Console.WriteLine(val.val);
+            val = val.next;
+        }
     }
 }
