3a68f22d7f65919fe63fcd0e60d9f35f74dae823
naflim <naflim@outlook.com>
2023-02-06 09:58:21 +0800

    2331. 计算布尔二叉树的值

---
+++ EvaluateTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 2331. 计算布尔二叉树的值
+    /// </summary>
+    internal class EvaluateTree
+    {
+        public static bool Run(TreeNode root)
+        {
+            switch (root.val)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return true;
+                case 2:
+                    return Run(root.left) || Run(root.right);
+                case 3:
+                    return Run(root.left) && Run(root.right);
+                default: return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,8 @@ class Program
 {
     public static void Main()
     {
-        //TreeNode root = new(1, new(2, new(4, new(8), new(9)), new(5, new(10), new(11))), new(3, new(6), new(7)));
-        //Console.WriteLine(BtreeGameWinningMove.Run(root, 11, 3));
-        TreeNode root = new(1, new(2, new(4), new(5)), new(3));
-        Console.WriteLine(BtreeGameWinningMove.Run(root, 5, 1));
-        //TreeNode root = new(1,new(2),new(3));
-        //Console.WriteLine(BtreeGameWinningMove.Run(root, 7, 3));
-        //TreeNode root = new(6, new(3, new(7), new(4, null, new(2, null, new(1, null, new(5))))), null);
-        //Console.WriteLine(BtreeGameWinningMove.Run(root, 7, 3));
+        //TreeNode root = new(2, new(1), new(3,new(0), new(1)));
+        TreeNode root = new(0);
+        Console.WriteLine(EvaluateTree.Run(root));
     }
 }
