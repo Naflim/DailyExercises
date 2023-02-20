0459c917a9a56474c440e5b90cdb1e6d1be7feeb
naflim <naflim@outlook.com>
2023-02-20 09:24:21 +0800

    2347. 最好的扑克手牌

---
+++ 2000/300/BestHand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 2347. 最好的扑克手牌
+    /// </summary>
+    internal class BestHand
+    {
+        public static string Run(int[] ranks, char[] suits)
+        {
+            Dictionary<int,int>rankDic = new Dictionary<int,int>();
+            Dictionary<char,int> suitDic = new Dictionary<char,int>();
+
+            foreach (int rank in ranks) 
+            {
+                if (rankDic.ContainsKey(rank)) rankDic[rank]++;
+                else rankDic[rank] = 1;
+            }
+
+            foreach (char suit in suits) 
+            {
+                if (suitDic.ContainsKey(suit)) suitDic[suit]++;
+                else suitDic[suit] = 1;
+            }
+
+            if (suitDic.Values.Count(v => v >= 5) > 0) return "Flush";
+            else if (rankDic.Values.Count(v => v >= 3) > 0) return "Three of a Kind";
+            else if (rankDic.Values.Count(v => v == 2) > 0) return "Pair";
+            else return "High Card";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,12 +8,8 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(Largest1BorderedSquare.Run(new int[][]
-        {
-            //new int[]{1,1,1 },
-            //new int[]{1,0,1 },
-            //new int[]{1,1,1 },
-            new int[]{1,1,0,0},
-        }));
+        Console.WriteLine(BestHand.Run(new int[] { 13, 2, 3, 1, 9 }, new char[] { 'a', 'a', 'a', 'a', 'a' }));
+        Console.WriteLine(BestHand.Run(new int[] { 4, 4, 2, 4, 4 }, new char[] { 'd', 'a', 'a', 'b', 'c' }));
+        Console.WriteLine(BestHand.Run(new int[] { 10, 10, 2, 12, 9 }, new char[] { 'a', 'b', 'c', 'a', 'd' }));
     }
 }
