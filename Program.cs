05aaa8b682bb136ae253b253bdbf8ce3bb07e2c7
naflim <naflim@outlook.com>
2023-03-02 10:34:29 +0800

    面试题 05.02. 二进制数转字符串

--- Program.cs
+++ Program.cs
@@ -8,22 +8,6 @@ class Program
 {
     public static void Main()
     {
-        var arr = LargestLocal.Run(new int[][]
-       {
-            new int[]{9,9,8,1},
-            new int[]{5,6,2,6},
-            new int[]{8,2,6,4},
-            new int[]{6,2,2,2},
-       }).ToArray();
-
-        for (int i = 0; i < arr.Length; i++)
-        {
-            for (int j = 0; j < arr.Length; j++)
-            {
-                Console.Write($"{arr[i][j]},");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine(PrintBin.Run(0.1));
     }
-
 }
---
+++ Special/PrintBin.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 面试题 05.02. 二进制数转字符串
+    /// </summary>
+    internal class PrintBin
+    {
+        public static string Run(double num)
+        {
+            double remainder = num % 1;
+            string integerBin = Convert.ToString((int)num, 2);
+            if (remainder == 0) return integerBin;
+
+            string decimalBin = string.Empty;
+            int decimalLen = 32 - integerBin.Length;
+
+            for (int i = 0; i < decimalLen; i++)
+            {
+                remainder *= 2;
+
+                if (remainder > 1)
+                {
+                    remainder %= 1;
+                    decimalBin += "1";
+                }
+                else if (remainder < 1)
+                {
+                    decimalBin += "0";
+                }
+                else
+                {
+                    remainder = 0;
+                    decimalBin += "1";
+                    break;
+                }
+            }
+
+            if (remainder == 0) return $"{integerBin}.{decimalBin}";
+            else return "ERROR";
+        }
+    }
+}
