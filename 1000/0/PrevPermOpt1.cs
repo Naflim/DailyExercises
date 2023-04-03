33a3a3c02f91e32da5f77d61b91b361d71c8e215
naflim <naflim@outlook.com>
2023-04-03 11:19:38 +0800

    1053. 交换一次的先前排列

---
+++ 1000/0/PrevPermOpt1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1053. 交换一次的先前排列
+    /// </summary>
+    public class PrevPermOpt1
+    {
+        public static int[] Run(int[] arr)
+        {
+            int len = arr.Length;
+
+            int left = int.MinValue, right = -1, rightNum = int.MinValue;
+            for (int i = len - 1; i > 0; i--)
+            {
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        if (j > left || (j==left && arr[i] > rightNum) || (j==left && arr[i] == rightNum && i < right))
+                        {
+                            left = j;
+                            right = i;
+                            rightNum = arr[i];
+                        }
+                        break;
+                    }
+                }
+            }
+
+            if (left != int.MinValue) (arr[left], arr[right]) = (arr[right], arr[left]);
+            return arr;
+        }
+    }
+}
