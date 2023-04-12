2c6d4b2283a8a9d0fcb3c51e5576e1ba971613a3
naflim <naflim@outlook.com>
2023-04-12 10:26:14 +0800

    1147. 段式回文

---
+++ 1000/100/LongestDecomposition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1147. 段式回文
+    /// </summary>
+    public class LongestDecomposition
+    {
+        public static int Run(string text)
+        {
+            int len = text.Length / 2;
+
+            int result = 0;
+            string head = string.Empty;
+            string end = string.Empty;
+
+            for (int i = 0; i<len; i++)
+            {
+                head += text[i];
+                end = text[text.Length - i - 1] + end;
+
+                if (head == end)
+                {
+                    result++;
+                    head = string.Empty;
+                    end = string.Empty;
+                }
+            }
+
+            result *= 2;
+            if (!string.IsNullOrEmpty(head) || text.Length % 2 == 1) result++;
+            return result;
+        }
+    }
+}
