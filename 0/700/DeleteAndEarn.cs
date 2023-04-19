4c1155f5cd22a5d6061ea01718db4c30de13f80c
naflim <naflim@outlook.com>
2023-04-19 11:57:49 +0800

    740. 删除并获得点数

---
+++ 0/700/DeleteAndEarn.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 740. 删除并获得点数
+    /// </summary>
+    internal class DeleteAndEarn
+    {
+        public static int Run(int[] nums)
+        {
+            if(nums.Length == 0) return 0;
+            
+
+            SortedDictionary<int, int> dic = new SortedDictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i])) dic[nums[i]] += nums[i];
+                else dic[nums[i]] = nums[i];
+            }
+
+            if (dic.Count == 1) return dic.Values.First();
+
+            int[] dp = new int[dic.Count];
+            dp[0] = dic.Values.First();
+            int prev = dic.Keys.First();
+            var cache = dic.ElementAt(1);
+            if (prev + 1 == cache.Key)
+            {
+                if (cache.Value > dp[0])
+                {
+                    dp[1] = cache.Value;
+                    prev = cache.Key;
+                }
+                else
+                {
+                    dp[1] = dp[0];
+                }
+            }
+            else
+            {
+                dp[1] = dp[0] + cache.Value;
+                prev = cache.Key;
+            }
+
+            for (int i = 2; i < dic.Count; i++)
+            {
+                var item = dic.ElementAt(i);
+                if(prev + 1 == item.Key)
+                {
+                    if(dp[i-1] <  dp[i - 2] + item.Value)
+                    {
+                        dp[i] = dp[i - 2] + item.Value;
+                        prev = item.Key;
+                    }
+                    else
+                    {
+                        dp[i] = dp[i - 1];
+                    }
+                }
+                else
+                {
+                    dp[i] = dp[i-1] + item.Value;
+                    prev = item.Key;
+                }
+            }
+            return dp.Last();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ class Program
 {
     public static void Main()
     {
-        
-
-        Console.WriteLine(CountDaysTogether.Run("08-15", "08-18","08-16","08-16"));
+        Console.WriteLine(DeleteAndEarn.Run(new int[] { 8, 3, 4, 7, 6, 6, 9, 2, 5, 8, 2, 4, 9, 5, 9, 1, 5, 7, 1, 4 }));
     }
 }
