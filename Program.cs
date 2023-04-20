efa9f3a6d7a6d4cf6a6e5aa516bafd7d0d5712d6
naflim <naflim@outlook.com>
2023-04-20 18:23:29 +0800

    62. 不同路径

--- Program.cs
+++ Program.cs
@@ -8,6 +8,6 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(DeleteAndEarn.Run(new int[] { 8, 3, 4, 7, 6, 6, 9, 2, 5, 8, 2, 4, 9, 5, 9, 1, 5, 7, 1, 4 }));
+        Console.WriteLine(UniquePaths.Run(3, 7));
     }
 }
