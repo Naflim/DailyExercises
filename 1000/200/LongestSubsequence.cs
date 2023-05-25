40e28c5d2f6352c066c33bb41f3e5398f782d2a0
naflim <naflim@outlook.com>
2023-05-25 10:36:13 +0800

    1218. 最长定差子序列 2451. 差值数组不同的字符串

---
+++ 1000/200/LongestSubsequence.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1218. 最长定差子序列
+    /// </summary>
+    internal class LongestSubsequence
+    {
+        public static int Run(int[] arr, int difference)
+        {
+            int[] dp = new int[arr.Length];
+            Array.Fill(dp, 1);
+
+            for (int i = 1; i < arr.Length; i++) 
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[j] + difference == arr[i])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                    else
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j]);
+                    }
+                }
+            }
+
+            return dp[^1];
+        }
+
+        public static int Run2(int[] arr, int difference)
+        {
+            Dictionary<int,int> dp = new Dictionary<int,int>();
+            foreach (int v in arr) 
+            {
+                int prve = dp.ContainsKey(v - difference) ? dp[v - difference] : 0;
+                dp[v] = prve + 1;
+            }
+
+            return dp.Values.Max();
+        }
+    }
+}
---
+++ 2000/400/OddString.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 2451. 差值数组不同的字符串
+    /// </summary>
+    internal class OddString
+    {
+        public static string Run(string[] words)
+        {
+            int[] a = GetInts(words[0]);
+            int[] b = GetInts(words[1]);
+
+            if(Enumerable.SequenceEqual(a, b))
+            {
+                for (int i = 2; i < words.Length; i++)
+                {
+                    if (!Enumerable.SequenceEqual(a, GetInts(words[i])))
+                    {
+                        return words[i];
+                    }
+                }
+
+                return string.Empty;
+            }
+            else
+            {
+                return Enumerable.SequenceEqual(a, GetInts(words[2]))? words[1] : words[0];
+            }
+        }
+
+        private static int[] GetInts(string str)
+        {
+            int[] ints = new int[str.Length - 1];
+            for (int i = 0; i < ints.Length; i++) 
+            {
+                ints[i] = str[i + 1] - str[i];
+            }
+
+            return ints;
+        } 
+    }
+}
