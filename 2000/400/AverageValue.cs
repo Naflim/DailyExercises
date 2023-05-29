c3f234ba8beca8469e89caa3f08688f3504ee180
naflim <naflim@outlook.com>
2023-05-29 12:05:00 +0800

    2455. 可被三整除的偶数的平均值

---
+++ 2000/400/AverageValue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 2455. 可被三整除的偶数的平均值
+    /// </summary>
+    internal class AverageValue
+    {
+        public static int Run(int[] nums)
+        {
+            int[] filter = nums.Where(n => n % 2 == 0 && n % 3 == 0).ToArray();
+
+            if(filter.Length == 0) return 0;
+            return filter.Sum() / filter.Length;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,36 +10,9 @@ class Program
 {
     public static void Main()
     {
-        //var val = new int[][]
-        //{
-        //    new int[] { 5,4 },
-        //    new int[] { 6,4 },
-        //    new int[] { 6,7 },
-        //    new int[] { 2,3 },
-        //};
-        // var val = new int[][]
-        //{
-        //     new int[] { 1,1 },
-        //     new int[] { 1,1 },
-        //     new int[] { 1,1 },
-        //};
 
-        string json = File.ReadAllText("C:\\Users\\Naflim\\Desktop\\data.txt");
-        int[][] val = JsonConvert.DeserializeObject<int[][]>(json);
-
-      //  var val = new int[][]
-      //{
-      //      new int[] { 4,5 },
-      //      new int[] { 4,6 },
-      //      new int[] { 6,7 },
-      //      new int[] { 2,3 },
-      //      new int[] { 1,1 },
-      //};
-
-        Console.WriteLine(MaxEnvelopes.Run2(val));
+        Console.WriteLine(AverageValue.Run(new int[] { 1, 2, 4, 7, 10 }));
         Console.ReadLine();
     }
 }
-//[[5,4],[6,4],[6,7],[2,3]]
-
-//[[4,5],[4,6],[6,7],[2,3],[1,1]]
+//[1,2,4,7,10]
