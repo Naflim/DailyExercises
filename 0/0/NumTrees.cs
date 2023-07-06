6b0ffc9dadacd7a725d3df0d4025f2ea09713903
naflim <naflim@outlook.com>
2023-07-06 10:43:06 +0800

    96. 不同的二叉搜索树

---
+++ 0/0/NumTrees.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 96. 不同的二叉搜索树
+    /// </summary>
+    internal class NumTrees
+    {
+        public static int Run(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int[] dp = new int[n + 1];
+                dp[0] = 1;
+                dp[1] = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        dp[i] += dp[j] * dp[i - 1 - j];
+                    }
+                }
+
+                return dp[^1];
+            }
+        }
+
+        public static int Run2(int n)
+        {
+            int C = 1;
+            for (int i = 0; i < n; ++i)
+            {
+                C = C * 2 * (2 * i + 1) / (i + 2);
+            }
+            return C;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
 {
     public static void Main()
     {
-        Console.WriteLine(MaxProfit4.Run(2,new int[] { 1, 2, 4, 7 }));
+        Console.WriteLine(NumTrees.Run2(5));
         Console.ReadLine();
     }
 }
