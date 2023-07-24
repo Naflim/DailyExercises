b3e8327f4fd26add9748159d6b0947d92192ef85
naflim <naflim@outlook.com>
2023-07-24 11:16:38 +0800

    771. 宝石与石头

---
+++ 0/700/NumJewelsInStones.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 771. 宝石与石头
+    /// </summary>
+    internal class NumJewelsInStones
+    {
+        public static int Run(string jewels, string stones)
+        {
+            HashSet<char> jewelHash = new HashSet<char>(jewels);
+            int result = 0;
+            foreach (var stone in stones)
+            {
+                if(jewelHash.Contains(stone))
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,18 +10,7 @@ class Program
 {
     public static void Main()
     {
-        //var tree = new TreeNode(3, new TreeNode(2, right: new TreeNode(3)), new TreeNode(3, right: new TreeNode(1)));
-        //var tree = new TreeNode(3, new TreeNode(4,new TreeNode(1) , new TreeNode(3)), new TreeNode(5, right: new TreeNode(1)));
-        //var tree = new TreeNode(4, new TreeNode(1, new TreeNode(2, new TreeNode(3))));
-        //var tree = new TreeNode(1,new TreeNode(2),new TreeNode(3));
-        //var tree = new TreeNode(-10,new TreeNode(9),new TreeNode(20,new TreeNode(15),new TreeNode(7)));
-        //var tree = new TreeNode(1, new TreeNode(-2, new TreeNode(1, null, new TreeNode(-1)), new TreeNode(3)), new TreeNode(-3, new TreeNode(-2)));
-        var tree new TreeNode(2,)
-        Console.WriteLine(MaxPathSum.Run(tree));
+        Console.WriteLine(NumJewelsInStones.Run("z", "ZZ"));
         Console.ReadLine();
     }
 }
-
-//[2,1,3,null,4]
-//[5,4,8,11,null,13,4,7,2,null,null,null,1]
-//[1,-2,-3,1,3,-2,null,-1]
