0e00746b29fcc1bb346a4fa0da046ab402b19c4a
naflim <naflim@outlook.com>
2023-07-24 11:03:21 +0800

    124. 二叉树中的最大路径和

---
+++ 0/100/MaxPathSum.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 124. 二叉树中的最大路径和
+    /// </summary>
+    internal class MaxPathSum
+    {
+        public static int Run(TreeNode root)
+        {
+            int result = int.MinValue;
+            HashSet<int> values = new HashSet<int>();
+            var val = GetMaxValue(root, values, ref result);
+            if (values.Any(v => v >= 0))
+                return result;
+            else
+                return values.Max();
+        }
+
+        public static (int, int) GetMaxValue(TreeNode root, HashSet<int> values, ref int maxValue)
+        {
+            int left = 0;
+            if (root.left != null)
+            {
+                var leftVal = GetMaxValue(root.left, values, ref maxValue);
+                var maxItem = Math.Max(leftVal.Item1, leftVal.Item2);
+                if (maxItem > 0) left += maxItem;
+                left += root.left.val;
+                if (left < 0) left = 0;
+            }
+
+            int right = 0;
+            if (root.right != null)
+            {
+                var rightVal = GetMaxValue(root.right, values, ref maxValue);
+                var maxItem = Math.Max(rightVal.Item1, rightVal.Item2);
+                if (maxItem > 0) right += maxItem;
+                right += root.right.val;
+                if (right < 0) right = 0;
+            }
+
+            values.Add(root.val);
+            maxValue = Math.Max(maxValue, left + right + root.val);
+            return (left, right);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,11 +13,15 @@ class Program
         //var tree = new TreeNode(3, new TreeNode(2, right: new TreeNode(3)), new TreeNode(3, right: new TreeNode(1)));
         //var tree = new TreeNode(3, new TreeNode(4,new TreeNode(1) , new TreeNode(3)), new TreeNode(5, right: new TreeNode(1)));
         //var tree = new TreeNode(4, new TreeNode(1, new TreeNode(2, new TreeNode(3))));
-        var tree = new TreeNode(2,new TreeNode(1,new TreeNode(4)),new TreeNode(3));
-
-        Console.WriteLine(Rob3.Run3(tree));
+        //var tree = new TreeNode(1,new TreeNode(2),new TreeNode(3));
+        //var tree = new TreeNode(-10,new TreeNode(9),new TreeNode(20,new TreeNode(15),new TreeNode(7)));
+        //var tree = new TreeNode(1, new TreeNode(-2, new TreeNode(1, null, new TreeNode(-1)), new TreeNode(3)), new TreeNode(-3, new TreeNode(-2)));
+        var tree new TreeNode(2,)
+        Console.WriteLine(MaxPathSum.Run(tree));
         Console.ReadLine();
     }
 }
 
 //[2,1,3,null,4]
+//[5,4,8,11,null,13,4,7,2,null,null,null,1]
+//[1,-2,-3,1,3,-2,null,-1]
