ad0d73b3f190757ad33a7390ff9fb0746a9f5b97
naflim <naflim@outlook.com>
2023-08-16 12:01:13 +0800

    790. 多米诺和托米诺平铺

---
+++ 0/700/NumTilings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 790. 多米诺和托米诺平铺
+    /// </summary>
+    internal class NumTilings
+    {
+        const int MOD = 0x3b9aca07;
+
+        public static int Run(int n)
+        {
+            int[][] dp = new int[n + 1][];
+            for (int i = 0; i <= n; i++)
+            {
+                dp[i] = new int[4];
+            }
+            dp[0][3] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                dp[i][0] = dp[i - 1][3];
+                dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+                dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+                dp[i][3] = (((dp[i - 1][0] + dp[i - 1][1]) % MOD + dp[i - 1][2]) % MOD + dp[i - 1][3]) % MOD;
+            }
+            return dp[n][3];
+        }
+    }
+}
