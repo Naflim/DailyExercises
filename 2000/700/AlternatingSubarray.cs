31d1a593bf51ce500b591d441c8abaab2318c2a6
naflim <naflim@outlook.com>
2024-01-23 21:28:23 +0800

    2765. 最长交替子数组

---
+++ 2000/700/AlternatingSubarray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises._2000._700
+{
+    /// <summary>
+    /// 2765. 最长交替子数组
+    /// </summary>
+    internal class AlternatingSubarray
+    {
+        public static int Run(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return -1;
+
+            int result = 0;
+            int count = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (count % 2 == 0)
+                {
+                    if (nums[i] -  nums[i - 1] == 1)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        result = Math.Max(result, count);
+                        count = 0;
+                    }
+
+                }
+                else
+                {
+                    if (nums[i] -  nums[i - 1] == -1)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        result = Math.Max(result, count);
+                        if (nums[i] -  nums[i - 1] == 1)
+                            count = 1;
+                        else
+                            count = 0;
+                    }
+                }
+            }
+
+            result = Math.Max(result, count);
+
+            if (result == 0)
+                return -1;
+            else
+                return result + 1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using DailyExercises;
+using DailyExercises._2000._700;
 using DailyExercises.Helper.Tree;
 using DailyExercises.LCP;
 using DailyExercises.Utils;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
@@ -15,33 +17,6 @@ class Program
 {
     public static void Main()
     {
-
-
-
-        //var val = LargestPathValue.Run4("abaca", new int[][]
-        //{
-        //    new int[]{0,1},
-        //    new int[]{0,2},
-        //    new int[]{2,3},
-        //    new int[]{3,4},
-        //});
-
-        var val = LongestAlternatingSubarray.Run(new int[]
-        {
-           1,2
-        },2);
-        //for (int i = 0; i < 100000; i++)
-        //{
-        //    Console.WriteLine(i);
-        //}
-        //return;
-        //string data1 = File.ReadAllText(@"C:\Users\Naflim\Desktop\data.txt");
-        //var data2 = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\data2.txt");
-        //var val = LargestPathValue.Run4(data1, data2);
-
-        //var data = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\people.txt");
-        //var val = LargestPathValue.Run2("nnllnzznn", data);
-
-        Console.WriteLine(val);
+        Console.WriteLine(AlternatingSubarray.Run(new int[] { 6, 12, 2, 3, 8, 9, 10, 10, 2, 1 }));
     }
 }
