6277439c48fd562c4e419797f5d35fa361e57778
Naflim <naflim@outlook.com>
2024-03-01 21:59:38 +0800

    2369. 检查数组是否存在有效划分

---
+++ 2000/300/ValidPartition.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 2369. 检查数组是否存在有效划分
+    /// </summary>
+    internal class ValidPartition
+    {
+        public static bool Run(int[] nums)
+        {
+            if(nums.Length < 2)
+                return false;
+
+            bool[] dp = new bool[nums.Length];
+
+            if (nums[0] == nums[1])
+            {
+                dp[1] = true;
+            }
+
+
+            if(nums.Length  == 2)
+                return dp[1];
+
+            
+            
+
+            for (int i = 2; i < nums.Length; i++)
+            {
+                int prev1 = nums[i - 2];
+                int prev2 = nums[i - 1];
+                int now = nums[i];
+
+
+                if (prev2 == now && dp[i - 2])
+                {
+                    dp[i] = true;
+                    continue;
+                }
+
+                if(prev1 == prev2 && prev1 == now)
+                {
+                    if(i - 3 >= 0)
+                    {
+                        if (dp[i - 3])
+                            dp[i] = true;
+                    }
+                    else
+                    {
+                        dp[i] = true;
+                    }
+                } 
+
+                if(prev1 +1 == prev2 && prev2 +1 == now)
+                {
+                    if (i - 3 >= 0)
+                    {
+                        if (dp[i - 3])
+                            dp[i] = true;
+                    }
+                    else
+                    {
+                        dp[i] = true;
+                    }
+                }
+            }
+
+            return dp.Last();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,43 +17,10 @@ class Program
 {
     public static void Main()
     {
-        //  int[][] edges = new int[][]
-        //  {
-        //        new int[] {0,1},
-        //        new int[] { 1,2},
-        //        new int[] {1,3},
-        //        new int[] { 4,2},
-        //  };
 
-        //  int[][] guesses = new int[][]
-        //{
-        //        new int[] {1,3},
-        //        new int[] { 0,1},
-        //        new int[] {1,0},
-        //        new int[] { 2,4},
-        //};
-
-        //  int[][] edges = new int[][]
-        // {
-        //           new int[] {1,3},
-        //          new int[] { 0,1},
-        //          new int[] {2,3},
-        //          new int[] { 3,4},
-        // };
-
-        //  int[][] guesses = new int[][]
-        //{
-        //          new int[] {1,0},
-        //          new int[] { 3,4},
-        //          new int[] {2,1},
-        //          new int[] {3,2},
-        //};
-
-        int[][] edges = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\data.txt");
-
-        int[][] guesses = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\data2.txt");
-
-
-        Console.WriteLine(RootCount.Run2(edges, guesses, 50000));
+        Console.WriteLine(ValidPartition.Run(new int[]
+        {
+           993335,993336,993337,993338,993339,993340,993341
+        }));
     }
 }
