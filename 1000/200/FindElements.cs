2dc4f3919e7a2a37d8d06449834a5857265570e2
naflim <naflim@outlook.com>
2024-03-12 17:33:05 +0800

    1261. 在受污染的二叉树中查找元素

---
+++ 1000/200/FindElements.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 1261. 在受污染的二叉树中查找元素
+    /// </summary>
+    internal class FindElements
+    {
+        private TreeNode _root;
+        private HashSet<int> _values;
+
+        public FindElements(TreeNode root)
+        {
+            _root = root;
+            _values = new HashSet<int>();
+
+            root.val = 0;
+            _values.Add(0);
+
+            if(root.left != null) 
+                Reduction(root,true);
+
+            if (root.right != null)
+                Reduction(root, false);
+        }
+
+        public void Reduction(TreeNode node, bool isLeft)
+        {
+            TreeNode now;
+            if(isLeft) 
+            {
+                Debug.Assert(node.left != null);
+                now = node.left;
+                now.val = 2 * node.val + 1;
+            }
+            else
+            {
+                Debug.Assert(node.right != null);
+                now = node.right;
+                now.val = 2 * node.val + 2;
+            }
+
+            _values.Add(now.val);
+
+            if(now.left != null)
+                Reduction(now, true);
+
+            if (now.right != null) 
+                Reduction(now, false);
+        }
+
+        public bool Find(int target)
+        {
+            return _values.Contains(target);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,43 +17,14 @@ class Program
 {
     public static void Main()
     {
-        //  int[][] edges = new int[][]
-        //  {
-        //        new int[] {0,1},
-        //        new int[] { 1,2},
-        //        new int[] {1,3},
-        //        new int[] { 4,2},
-        //  };
+        //TreeNode root = new TreeNode(-1, null, new TreeNode(-1));
 
-        //  int[][] guesses = new int[][]
-        //{
-        //        new int[] {1,3},
-        //        new int[] { 0,1},
-        //        new int[] {1,0},
-        //        new int[] { 2,4},
-        //};
+        TreeNode root = new TreeNode(-1, new TreeNode(-1,new TreeNode(-1),new TreeNode(-1)), new TreeNode(-1));
 
-        //  int[][] edges = new int[][]
-        // {
-        //           new int[] {1,3},
-        //          new int[] { 0,1},
-        //          new int[] {2,3},
-        //          new int[] { 3,4},
-        // };
+        FindElements demo = new FindElements(root);
 
-        //  int[][] guesses = new int[][]
-        //{
-        //          new int[] {1,0},
-        //          new int[] { 3,4},
-        //          new int[] {2,1},
-        //          new int[] {3,2},
-        //};
-
-        int[][] edges = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\data.txt");
-
-        int[][] guesses = IOUtils.GetArrsByFile(@"C:\Users\Naflim\Desktop\data2.txt");
-
-
-        Console.WriteLine(RootCount.Run2(edges, guesses, 50000));
+        Console.WriteLine(demo.Find(1));
+        Console.WriteLine(demo.Find(3));
+        Console.WriteLine(demo.Find(5));
     }
 }
