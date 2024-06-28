8e3b583499b40214178a86143edbfdb82b8329ec
naflim <naflim@outlook.com>
2024-06-28 16:38:42 +0800

    调整方法名

--- LCP/MaxmiumScore.cs
+++ LCP/MaxmiumScore.cs
@@ -22,48 +22,37 @@ namespace DailyExercises.LCP
 
             int sum = leftArr.Sum();
 
-            if (IsOdd(sum))
+            if (IsEven(sum))
                 return sum;
 
             if (!rightArr.Any())
                 return 0;
 
-            bool hasOdd = rightArr.Any(IsOdd);
-            bool hasEven = rightArr.Any(IsEven);
+            bool hasOdd = rightArr.Any(IsEven);
+            bool hasEven = rightArr.Any(IsOdd);
 
             if (hasOdd && hasEven)
             {
                 int result1 = 0;
-                if (leftArr.Any(IsEven))
+                if (leftArr.Any(IsOdd))
                 {
                     result1 = sum;
-                    result1 -= leftArr.Last(IsEven);
-                    result1 += rightArr.First(IsOdd);
+                    result1 -= leftArr.Last(IsOdd);
+                    result1 += rightArr.First(IsEven);
                 }
 
                 int result2 = 0;
-                if (leftArr.Any(IsOdd))
+                if (leftArr.Any(IsEven))
                 {
                     result2 = sum;
-                    result2 -= leftArr.Last(IsOdd);
-                    result2 += rightArr.First(IsEven);
+                    result2 -= leftArr.Last(IsEven);
+                    result2 += rightArr.First(IsOdd);
                 }
 
                 return Math.Max(result1, result2);
             }
 
             if (hasOdd)
-            {
-                if (!leftArr.Any(IsEven))
-                    return 0;
-
-                sum -= leftArr.Last(IsEven);
-                sum += rightArr.First();
-
-                return sum;
-            }
-
-            if (hasEven)
             {
                 if (!leftArr.Any(IsOdd))
                     return 0;
@@ -74,10 +63,21 @@ namespace DailyExercises.LCP
                 return sum;
             }
 
+            if (hasEven)
+            {
+                if (!leftArr.Any(IsEven))
+                    return 0;
+
+                sum -= leftArr.Last(IsEven);
+                sum += rightArr.First();
+
+                return sum;
+            }
+
             return 0;
         }
 
-        public static bool IsOdd(int num) { return num % 2 == 0; }
-        public static bool IsEven(int num) { return num % 2 == 1; }
+        public static bool IsEven(int num) { return num % 2 == 0; }
+        public static bool IsOdd(int num) { return num % 2 == 1; }
     }
 }
