9e5c0f5f24eeedd706634d13ba91a2d82dec1ef2
naflim <naflim@outlook.com>
2024-11-14 17:46:05 +0800

    287. 寻找重复数

---
+++ 0/200/FindDuplicate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyExercises
+{
+    /// <summary>
+    /// 287. 寻找重复数
+    /// </summary>
+    internal class FindDuplicate
+    {
+        public static int Run(int[] nums)
+        {
+            int len = nums.Length;
+            int n = len - 1;
+
+            int result = 0;
+            for (int i = 0; i < 32; i++)
+            {
+                int defVal = 0;
+                int mask = 1 << i;
+                int numsVal = nums[0] & mask;
+
+                for (int j = 1; j < len; j++)
+                {
+                    defVal += j & mask;
+                    numsVal += nums[j] & mask;
+                }
+
+                if(numsVal > defVal)
+                    result |= 1 << i;
+            }
+
+            return result;
+        }
+    }
+}
